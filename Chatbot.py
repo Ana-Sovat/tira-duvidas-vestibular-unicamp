e1784798b516d71d4e95537405096772c87944b1
Ana-Sovat <38082922+Ana-Sovat@users.noreply.github.com>
2023-11-04 05:07:52 +0000

    Dummy version without secret key

--- Chatbot.py
+++ Chatbot.py
@@ -5,18 +5,20 @@ import pandas as pd
 
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-
-#lê os dados
-tabela = pd.read_csv("Vestibular.csv",index_col=0)
-
-#cria os embeddings e coloca no chroma
-chroma_unicamp = Chroma.from_texts(texts=tabela.Textos.to_list(),embedding=OpenAIEmbeddings())
-
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 
-modelo = ChatOpenAI()
+#lê os dados
+tabela = pd.read_csv("Vestibular.csv",index_col=0)
+
+#TODO: falta a chave
+
+#cria os embeddings e coloca no chroma
+#chroma_unicamp = Chroma.from_texts(texts=tabela.Textos.to_list(),embedding=OpenAIEmbeddings())
+
+#cria a conexão com o modelo
+#modelo = ChatOpenAI()
 
 prompt_template_unicamp = """Você é um assistente virtual que tem por objetivo sanar dúvidas sobre o Vestibular Unicamp 2024.
 As respostas para as perguntas devem ser baseadas nas informações contidas no documento oficial da universidade (Resolução GR-031/2023, de 13/07/2023).
@@ -28,7 +30,7 @@ Pergunta: {question}"""
 
 template = PromptTemplate(template=prompt_template_unicamp, input_variables=["context","question"])
 
-respondedor_de_perguntas = RetrievalQA.from_chain_type(llm=modelo,retriever=chroma_unicamp.as_retriever(),chain_type_kwargs={"prompt": template},return_source_documents=True)
+#respondedor_de_perguntas = RetrievalQA.from_chain_type(llm=modelo,retriever=chroma_unicamp.as_retriever(),chain_type_kwargs={"prompt": template},return_source_documents=True)
 
 LOGGER = get_logger(__name__)
 
@@ -49,10 +51,11 @@ def run():
         with st.chat_message("user"):
             st.write(f"{pergunta}")
 
-        resposta = respondedor_de_perguntas(pergunta)['result']
+        #resposta = respondedor_de_perguntas(pergunta)['result']
 
         with st.chat_message("assistant"):
-            st.write(resposta)
+            #st.write(resposta)
+            st.write("Não sei, ainda estou estudando.")
 
 
 
