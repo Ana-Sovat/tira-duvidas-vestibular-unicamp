1b42f3370578a0daa521a5d22e6c03f12d0f9121
Ana-Sovat <38082922+Ana-Sovat@users.noreply.github.com>
2023-11-04 22:13:05 +0000

    Basic working version

--- Hello.py
+++ Hello.py
@@ -1,3 +1,5 @@
+import os
+
 import streamlit as st
 from streamlit.logger import get_logger
 
@@ -12,14 +14,16 @@ from langchain.chat_models import ChatOpenAI
 #lê os dados
 tabela = pd.read_csv("Vestibular.csv",index_col=0)
 
-#TODO: falta a chave
+#pega a chave
+os.environ['OPENAI_API_KEY'] = st.secrets["API_SECRET_KEY"]
 
 #cria os embeddings e coloca no chroma
-#chroma_unicamp = Chroma.from_texts(texts=tabela.Textos.to_list(),embedding=OpenAIEmbeddings())
+chroma_unicamp = Chroma.from_texts(texts=tabela.Textos.to_list(),embedding=OpenAIEmbeddings())
 
 #cria a conexão com o modelo
-#modelo = ChatOpenAI()
+modelo = ChatOpenAI()
 
+#cria o template do prompt, para incorporar o contexto
 prompt_template_unicamp = """Você é um assistente virtual que tem por objetivo sanar dúvidas sobre o Vestibular Unicamp 2024.
 As respostas para as perguntas devem ser baseadas nas informações contidas no documento oficial da universidade (Resolução GR-031/2023, de 13/07/2023).
 Se uma pergunta não puder ser respondida com base no seu conhecimento, você deve dizer que não possui informações suficientes.
@@ -30,13 +34,14 @@ Pergunta: {question}"""
 
 template = PromptTemplate(template=prompt_template_unicamp, input_variables=["context","question"])
 
-#respondedor_de_perguntas = RetrievalQA.from_chain_type(llm=modelo,retriever=chroma_unicamp.as_retriever(),chain_type_kwargs={"prompt": template},return_source_documents=True)
+#cria o chatbot
+respondedor_de_perguntas = RetrievalQA.from_chain_type(llm=modelo,retriever=chroma_unicamp.as_retriever(),chain_type_kwargs={"prompt": template},return_source_documents=True)
 
 LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="ChatBot"
+        page_title="Tira-Dúvidas Automático"
     )
 
     st.title("Tira-Dúvidas Automático")
@@ -51,11 +56,12 @@ def run():
         with st.chat_message("user"):
             st.write(f"{pergunta}")
 
-        #resposta = respondedor_de_perguntas(pergunta)['result']
+        #envia a pergunta ao ChatGPT
+        resposta = respondedor_de_perguntas(pergunta)['result']
 
         with st.chat_message("assistant"):
-            #st.write(resposta)
-            st.write("Não sei, ainda estou estudando.")
+            st.write(resposta)
+            #st.write("Não sei, ainda estou estudando.")
 
 
 
